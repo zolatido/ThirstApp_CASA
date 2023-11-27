ba27a87a73a07fbc50ba1bb7468ae4fd72bd5aee
ArwenAlcaraz <142982183+ArwenAlcaraz@users.noreply.github.com>
2023-11-27 22:07:02 +0800

    Calendar and line chart history.js

--- mob/mobapp/src/pages/history.js
+++ mob/mobapp/src/pages/history.js
@@ -1,52 +1,23 @@
 //history.js
 
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  ImageBackground,
-  StyleSheet,
-} from 'react-native';
+import { View, Text, Image, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import { LineChart } from 'react-native-chart-kit';
 
-//assets
-
-import { Modal } from 'react-native';
-
-
-
-//assets
-import logodrink from '../assets/logotrans.png';
+//import logodrink from '../assets/logotrans.png';
 import bg from '../assets/bg.png';
 
-export default function Dashboard({ navigation }) {
+export default function History({ navigation }) {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [chartData, setChartData] = useState([0, 0, 0, 0, 0, 0, 0]); // Modify the initial chart data
 
-  const [isModalVisible, setModalVisible] = useState(false);
-
-
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
-  };
-
-  const closeSlideInModal = () => {
-    setModalVisible(false);
-  };
-
-  const navigateToDashboard = () => {
-    setModalVisible(false); // Close the modal
-    navigation.navigate('Dashboard'); // Navigate to the Dashboard screen
-  };
-
-  const navigateToHistory = () => {
-    setModalVisible(false); // Close the modal
-    navigation.navigate('History'); // Navigate to the History screen
-  };
-
-  const navigateToSettings = () => {
-    setModalVisible(false); // Close the modal
-    navigation.navigate('Settings'); // Navigate to the Settings screen
+  const handleDateSelect = (day) => {
+    console.log('Selected date:', day.dateString);
+    // TODO: Fetch your chart data based on the selected date
+    // Replace the following line with your data fetching logic
+    setChartData([100, 150, 120, 200, 180, 160, 140]); // Dummy data, replace with your actual data
+    setSelectedDate(day.dateString);
   };
 
   return (
@@ -54,198 +25,126 @@ export default function Dashboard({ navigation }) {
       <ImageBackground source={bg} style={styles.backgroundImage}>
         <View style={styles.overlay}>
           <View style={styles.appContainer}>
-          <Text style={styles.headerMotto2}>History</Text>
+            <Text style={styles.headerMotto2}>Water Intake History</Text>
             <Text style={styles.headerMotto3}>Keep track of your thirsts</Text>
-            <Text style={styles.headerMotto3}>Weekly Report</Text>
-            <Text style={styles.headerMotto3}>Daily Report</Text>
-          <Image source={logodrink} style={styles.logo} />
-            <View style={styles.headerContainer}>
-             
+            <View style={styles.contentContainer}>
+              <View style={styles.headerContainer}></View>
+              <View style={styles.calendarContainer}>
+                <Text style={styles.selectedDateText}>{selectedDate}</Text>
+                <Calendar
+                  onDayPress={(day) => handleDateSelect(day)}
+                  markingType={'period'}
+                  markedDates={{
+                    '2023-01-01': { selected: true, marked: true },
+                    '2023-01-02': { selected: true, marked: true },
+                    '2023-01-03': { selected: true, marked: true },
+                  }}
+                />
+              </View>
+              <View style={styles.chartContainer}>
+                <LineChart
+                  data={{
+                    labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+                    datasets: [
+                      {
+                        data: chartData,
+                      },
+                    ],
+                  }}
+                  width={300}
+                  height={200}
+                  yAxisLabel=""
+                  yAxisSuffix=""
+                  yAxisInterval={1}
+                  chartConfig={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                    backgroundGradientFrom: 'rgba(255, 255, 255, 0.7)',
+                    backgroundGradientTo: 'rgba(255, 255, 255, 0.7)',
+                    decimalPlaces: 0,
+                    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                    style: {
+                      borderRadius: 16,
+                    },
+                    propsForDots: {
+                      r: '7',
+                      strokeWidth: '2',
+                      stroke: '#ffa726',
+                    },
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                  }}
+                />
+              </View>
             </View>
-           
-           
-            
           </View>
         </View>
       </ImageBackground>
-      <Modal
-        transparent={true}
-        visible={isModalVisible}
-        onBackdropPress={closeSlideInModal}
-        animationIn="slideInLeft"
-        animationOut="slideOutLeft"
-        onRequestClose={toggleModal}
-      >
-        <View style={styles.modalContainer}>
-          <Text style={styles.welcomeText}>Welcome</Text>
-          <TouchableOpacity onPress={navigateToDashboard}>
-            <Text style={styles.modalText2}>Dashboard</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={navigateToHistory}>
-            <Text style={styles.modalText2}>History</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={navigateToSettings}>
-            <Text style={styles.modalText2}>Settings</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  overlay: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
-  appContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    color: 'white',
-    left: -165,
-    bottom:450,
-    
-    paddingHorizontal: 10,
-  },
-  logo: {
-    height: 170, // size nung logo
-    width:170, // size nung loge
-    bottom: 300,
-    alignItems: 'center',
-    
-  },
-  header: {
-    fontSize: 20,
-    marginBottom: 10,
-    color: 'white',
-  },
-  //(HOME)started
-  headerMotto2: {
-    fontSize: 24,
-   // fontWeight: 'bold',
-    color: "white",
-    top: 50,
-  },
-
-  //Stay hydrated
-  headerMotto3: {
-    fontSize: 15,
-    color: "white",
-    top: 60,
-   
-    
-  },
-  meterContainer: {
-    height: 300,
-    width: 300,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  meter: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'lightgray',
-    borderRadius: 100,
-    overflow: 'hidden',
-  },
-  fill: {
-    width: '100%',
-    backgroundColor: 'blue',
-    position: 'absolute',
-    bottom: 0,
-  },
-  consumedText: {
-    fontSize: 18,
-    marginTop: 10,
-    color: 'white',
-  },
-  goalText: {
-    fontSize: 16,
-    marginTop: 10,
-    color: 'white',
-  },
-  touchableWaterBottle: {
-    width: 80,
-    height: 160,
-    marginTop: 20,
-  },
-  messageText: {
-    fontSize: 18,
-    color: 'green',
-    marginTop: 10,
-  },
-  historyContainer: {
-    maxHeight: 200,
-    width: 230,
-    marginTop: 20,
-    borderWidth: 1,
-    padding: 20,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-    borderRadius: 10,
-  },
-  historyRecord: {
-    marginBottom: 10,
-    color: 'white',
-  },
-  historyDate: {
-    fontSize: 16,
-    color: 'white',
-    //fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  historyText: {
-    fontSize: 16,
-    color: 'white',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-  button: {
-    flex: 1,
-    backgroundColor: '#8BADD3',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    margin: 5,
-  },
-
-  welcomeText: {
-    color: 'white',
-    fontSize: 30,
-  },
-
-  modalText2: {
-    color: 'white',
-    fontSize: 18,
-  },
-
-  buttonText: {
-    color: '#333',
-    fontSize: 18,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-  },
-  modalText: {
-    fontSize: 24,
-    color: 'white',
-    marginBottom: 20,
-  },
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    overlay: {
+      flex: 1,
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    },
+    backgroundImage: {
+      flex: 1,
+      resizeMode: 'cover',
+    },
+    appContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      color: 'white',
+      left: -165,
+      paddingHorizontal: 10,
+    },
+    headerMotto2: {
+      fontSize: 25,
+      color: 'white',
+      top: 20,
+    },
+    headerMotto3: {
+      fontSize: 15,
+      color: 'white',
+      top: 30,
+    },
+    contentContainer: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    calendarContainer: {
+      flex: 1,
+      flexDirection: 'column',
+      marginTop: 30,
+      alignItems: 'center',
+    },
+    selectedDateText: {
+      fontSize: 18,
+      color: 'white',
+      textAlign: 'center',
+      marginBottom: 10,
+    },
+    chartContainer: {
+        marginTop: 40,
+        borderRadius: 10,
+        padding: 20,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+    },
+  });
